a4bc8cd21d68e1ef707ac9780feb3025afc4b41d
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-11-12 15:09:40 +0900

    update articles controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,22 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+    @article = Article.new(params[:article])
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
